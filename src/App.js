124cfc40210964de2f0a30882b6ca8e172612bae
Pedyz <pdromartins11@gmail.com>
2024-07-25 23:14:40 -0300

    Added search input, song and artists lists

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import AuthBtn from './components/Auth/AuthBtn'
-import Style from "./styles.css"
+import SearchBar from './components/SearchBar/SearchBar'
+import Style from "./App.module.css"
 
 function App() {
     const [token, setToken] = useState('')
@@ -23,16 +24,41 @@ function App() {
         }
 
     }, [])
+
+    const [results, setResults] = useState([])
   
     return (
-        <div id="mainDiv">
+        <div id={Style.mainDiv}>
             {!token ? 
-            <div className='textDiv'>
+            <div className={Style.textDiv}>
                 <h2>Create your own Spotify playlist with Jammming</h2>
                 <AuthBtn/>
             </div>
             
-            : <h2></h2>
+            :
+            <div id={Style.container}>
+                <div className={Style.column}>
+                    <SearchBar onResults={setResults}/>
+                    <div className={Style.fullList}>
+                        <ul className={Style.songList}>
+                            {results.map(track => (
+                                <li key={track.id} className={Style.song}>
+                                    <img className={Style.albumImg} src={track.album.images[0].url}/>
+                                    <div className={Style.songTxt}>
+                                        <h2>{track.name}</h2>
+                                        <h3>{track.artists.map(artist => artist.name).join(', ')}</h3>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div className={Style.column}>
+
+                </div> 
+            </div>
             }
         </div>
             
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,37 @@
+import Styles from './SearchBar.module.css'
+import { useState } from 'react'
+
+function SearchBar( {onResults} ) {
+    const [search, setSearch] = useState('')
+    const token = localStorage.getItem('token')
+
+    const searchItem = async (e) => {
+        const newSearch = e.target.value
+        setSearch(newSearch)
+
+        if (newSearch.length > 2) {
+            try {
+                const response = await fetch(`https://api.spotify.com/v1/search?q=${newSearch}&type=track`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+
+                const data = await response.json()
+                onResults(data.tracks.items)
+            } catch (error) {
+                console.log(error)
+            }
+
+        } else {
+            onResults([])
+        }
+    }
+
+
+    return (
+        <input className={Styles.searchBar} onChange={searchItem}/>
+    )
+}
+
+export default SearchBar
